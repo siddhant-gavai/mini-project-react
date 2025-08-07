c74eb6865d3486d9adc85f97d1fc264c2fbb8b97
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-08-07 09:24:59 +0530

    some changes

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -5,38 +5,48 @@ import { useState } from "react";
 
 export default function SerchBox({ updateInfo }) {
   let [city, setCity] = useState("");
+  let [error, setError] = useState(false);
 
   const API_URL = "https://api.openweathermap.org/data/2.5/weather";
   const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
   let getWeatherInfo = async () => {
-    let response = await fetch(
-      `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
-    );
-    let jsonResponse = await response.json();
+    try {
+      let response = await fetch(
+        `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      let jsonResponse = await response.json();
 
-    let result = {
-      city: city,
-      temp: jsonResponse.main.temp,
-      tepMin: jsonResponse.main.temp_min,
-      temMax: jsonResponse.main.temp_max,
-      humidity: jsonResponse.main.humidity,
-      feelsLike: jsonResponse.main.feels_like,
-      weather: jsonResponse.weather[0].description,
-    };
-    console.log(result);
-    return result;
+      let result = {
+        city: city,
+        temp: jsonResponse.main.temp,
+        tepMin: jsonResponse.main.temp_min,
+        temMax: jsonResponse.main.temp_max,
+        humidity: jsonResponse.main.humidity,
+        feelsLike: jsonResponse.main.feels_like,
+        weather: jsonResponse.weather[0].description,
+      };
+      console.log(result);
+      return result;
+    } catch (err) {
+      throw err;
+    }
   };
 
   let handleChange = (e) => {
     setCity(e.target.value);
   };
+
   let handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(city);
-    setCity("");
-    let newInfo = await getWeatherInfo();
-    updateInfo(newInfo);
+    try {
+      e.preventDefault();
+      console.log(city);
+      setCity("");
+      let newInfo = await getWeatherInfo();
+      updateInfo(newInfo);
+    } catch (err) {
+      setError(true);
+    }
   };
   return (
     <div className="serbox-container">
@@ -55,6 +65,9 @@ export default function SerchBox({ updateInfo }) {
         <Button variant="outlined" type="submit" className="serbox-button">
           Search
         </Button>
+        {error && (
+          <p style={{ color: "red" }}>No such place Exist in the API</p>
+        )}
       </form>
     </div>
   );
